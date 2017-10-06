a493fed186613f7607377329736450e65cbd5610
Lawrence Thorpe <thorpe_lawrence@yahoo.co.uk>
2017-10-06 17:50:44 +0100

    Change to postgres

--- vids-psg/index.js
+++ vids-psg/index.js
@@ -1,8 +1,23 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const { Client } = require('pg');
+const app = express();
+const client = new Client({
+    host: 'db',
+    user: 'vids-psg',
+    password: 'vids-psg',
+    database: 'vids_psg',
+});
+client.connect();
 
 app.use('/', express.static('www'));
 
-app.listen(8080, function() {
+app.get('/database', async (req, res) => {
+    var response = await client.query('SELECT * from vids_psg');
+    res.send(response);
+    client.end();
+    res.send("<h2>Hello!</h2>");
+});
+
+app.listen(8080, () => {
     console.log('Listening on port 8080');
 });
