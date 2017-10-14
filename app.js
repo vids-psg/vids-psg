498c6494b73dbfd17215c9d7bb003ea366034a4b
Lawrence Thorpe <thorpe_lawrence@yahoo.co.uk>
2017-10-14 12:09:44 +0100

    Add password example (#4)
    * Add password template
    
    * Add zxcvbn and argon2 examples
    
    * Cleanup style on password-score

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var login = require('./routes/login');
 
 var app = express();
 
@@ -21,9 +22,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// static link to zxcvbn script in node_modules
+app.use('/zxcvbn', express.static(path.join(__dirname, 'node_modules/zxcvbn/dist')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
