753a5b7c6ad8cc1437a81c4f90c8b9a28d5926e2
Lawrence Thorpe <thorpe_lawrence@yahoo.co.uk>
2017-11-16 19:48:49 +0000

    Add docker

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 var client = new Client({
     user: 'vids_psg',
     password: 'vids_psg',
-    host: 'localhost',
+    host: 'db',
     database: 'vids_psg'
 });
 
---
+++ db/initdb.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    userid serial,
+    username varchar NOT NULL,
+    password varchar NOT NULL
+);
+
+-- CREATE TABLE videos (
+
+-- );
